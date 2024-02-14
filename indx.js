660415f6f8f38be443e20ebaf28887c7a09f7a53
Joh-Ishimwe <j.ishimwe3@alustudent.com>
2024-02-14 15:25:44 +0200

    sdfghj

---
+++ indx.js
@@ -0,0 +1,56 @@
+//let age=30;
+//console.log(age);
+/*let mixedArray = [0, 1, true, false, 1, 0];
+
+for (let value of mixedArray) {
+    if (typeof value === 'boolean') {
+        console.log(`Boolean value: ${value}`);
+    } else if (typeof value === 'number') {
+        console.log(`Numeric value: ${value}`);
+    }
+}*/
+
+
+/*color.push ("orange");
+console.log(color);
+color.pop(color);
+console.log(color);
+let secondcolor = color[1];
+console.log (secondcolor);
+let Doorangeexit = color.includes("red");
+console.log(Doorangeexit);*/
+
+
+/*for (let i = 0; i < color.length; i++) {
+  color.push ("orange");
+  console.log(color[i]);
+}
+*/
+/*let color = ["red", "green", "blue"];
+// Add "orange" in the array
+color.push("orange");
+ for (let i = 0; i < color.length; i++) {
+  //  console.log(color)
+console.log(color[i]);
+ }
+
+// Print the updated array
+//console.log(color);
+
+*/
+/*let number = [1,2,3,4,5,6];
+let doublenumber = number.map (number => number *2);
+console.log (doublenumber);
+
+let oddNumber = number.filter(number => number % 2 !== 0);
+console.log(oddNumber);*/
+/*let firstArray = [1, 2, 3];
+let secondArray = [4, 5, 6];
+let combinedArray = firstArray.concat(secondArray);
+console.log(combinedArray);*/
+
+
+let duplicates = [1, 2, 2, 3, 4, 4, 5];
+let uniqueArray = [...new Set(duplicates)];
+console.log(uniqueArray); 
+consolelocalStorage
