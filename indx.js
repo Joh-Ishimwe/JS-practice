1d680da03f58bdd5010f78f350e719a0f43ae288
Joh-Ishimwe <j.ishimwe3@alustudent.com>
2024-02-14 11:22:21 +0200

    my firstday in JS class

---
+++ indx.js
@@ -0,0 +1,39 @@
+//let age=30;
+//console.log(age);
+/*let mixedArray = [0, 1, true, false, 1, 0];
+
+for (let value of mixedArray) {
+    if (typeof value === 'boolean') {
+        console.log(`Boolean value: ${value}`);
+    } else if (typeof value === 'number') {
+        console.log(`Numeric value: ${value}`);
+    }
+}*/
+
+
+/*color.push ("orange");
+console.log(color);
+color.pop(color);
+console.log(color);
+let secondcolor = color[1];
+console.log (secondcolor);
+let Doorangeexit = color.includes("red");
+console.log(Doorangeexit);*/
+
+
+/*for (let i = 0; i < color.length; i++) {
+  color.push ("orange");
+  console.log(color[i]);
+}
+*/
+let color = ["red", "green", "blue"];
+
+color.push("orange");
+// Add "orange" in the array
+ for (let i = 0; i < color.length; i++) {
+color.push("orange");
+console.log(color[i]);
+ }
+
+// Print the updated array
+//console.log(color);
